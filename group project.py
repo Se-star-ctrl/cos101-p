1c2d0e8e1e53c006e7b964e38bf4f450a86fdd78
Se-star-ctrl <seyi.akinbowale@binghamuni.edu.ng>
2026-01-22 07:59:44 +0100

    Add files via upload

---
+++ group project.py
@@ -0,0 +1,23 @@
+import streamlit as st
+French = {
+        'Hello' : 'Bonjour',
+        'Thank' : 'Merci',
+        'Water' : 'Eau',
+        'Sun' : 'Soleil',
+        'Oui' : 'Yes',
+        'Dog' : 'Chien',
+        'Cat' : 'Chat',
+        'Car' : 'Voiture',
+        'Tree' : 'Arbre',
+        'Road' : 'Route',
+        'School' : 'Ecole',
+        'Friend' : 'Ami',
+        'Phone' : 'Telephone',
+        'Chair' : 'Chaise',
+        'Table' : 'Table',
+        'Light' : 'Lumiere',
+        'Child' : 'Enfant',
+        'Work' : 'Travail',
+        'Love' : 'Amour',
+        'Music' : 'Musique',
+    }
